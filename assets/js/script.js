88c72544e4927382c6a960a81503624da19d2efa
Nainoa Dinson <nd808@msn.com>
2023-09-27 09:34:07 -1000

    created a TODO list for project

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//TODO: create a start button
+    //TODO: start button must start a timer
+    //TODO: start button must present a question
+//TODO: present another question when user answers the previous one
+//TODO: create function that subtracts time from clock when answer is wrong
+//TODO: when the timer reaches 0 then the game is over
+//TODO: when all the questions are answered then the game is over
+//TODO: display score at end
+//TODO: create input that user can save his/her initials and score 
